1b1e6d045dd6239dfa340d98192b463a11e1c9c7
rocko <rocko@141.64.165.15>
2015-01-06 11:46:54 +0100

    Aufgabe 3 (Sender) läuft

---
+++ nachrichtenkanal/src/nachrichtenkanal/Sender.java
@@ -0,0 +1,48 @@
+package nachrichtenkanal;
+
+import cs101.io.Console;
+import cs101.lang.Animate;
+import cs101.lang.AnimatorThread;
+
+public class Sender implements Animate{
+	
+	public static TextContainer kanal1;
+	
+	public Sender() {
+
+	}
+	
+	public Sender(TextContainer kanal1) {
+		System.out.println("Bitte erste Nachricht eingeben:");
+		this.kanal1 = kanal1;
+		
+	}
+		
+	public static void lesen() {
+		System.out.println("NEU:");
+		String text = Console.readln();
+		kanal1.enter(text);
+	
+	}
+	
+	
+	public static void main(String[] args) {
+		TextContainerPrototype kanal1 = new TextContainerPrototype(4);
+		AnimatorThread aniSender = new AnimatorThread(new Sender(kanal1));
+		aniSender.startExecution();
+		
+		
+	}
+
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+		lesen();
+	}
+	
+}
+	
+
+
+
